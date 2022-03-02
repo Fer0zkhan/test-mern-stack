363f1330680790007e1552ec210c0625e96af793
Feroz Khan <muhammad.feroz@devsinc.com>
2022-03-02 16:18:44 +0500

    mongoDB configrations and make REST-API for goals and also completed crud operations

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 const { errorHandler } = require('./middleware/errorMiddleware')
+
+connectDB();
 
 const app = express();
 
@@ -12,5 +16,5 @@ app.use('/api/goals', require('./routes/goalRoutes'));
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Server running at port = ${port}`));
+app.listen(port, () => console.log(`Server running at port = ${port}`.blue.underline));
 
